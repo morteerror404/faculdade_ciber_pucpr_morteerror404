58aae3e46940922d762190309f0ea0f655e07034
ZjpffrotaZ <frotatv@gmail.com>
2023-08-30 21:07:09 -0300

    atualizacao bob.py 2.0

--- criptografia/somativa1/somativa1/Alice.py
+++ criptografia/somativa1/somativa1/Alice.py
@@ -3,7 +3,8 @@ import RSALib as RSA
 import AESLib as AES
 
 # 1) ALICE GERA AS CHAVES PÚBLICAS E PRIVADA
-
+chavePriObj, chavePriPEM = RSA.geraChavePrivada(2048, "chavepriva.pem")
+chavePubObj, chavePubPEM = RSA.geraChavePublica(chavePriObj, "chavepublica.pem")
 print('ESTA TELA PERTENCE A ALICE')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,6 +12,7 @@ s.bind(('', 9999))
 
 while True:
     
+    print(f'Aguardando um HELLO ...') 
     data, addr = s.recvfrom(1024) 
     datastr = data.decode()   
     print(f'RECEBI {datastr} de {addr}')
@@ -18,23 +20,25 @@ while True:
     if datastr == "HELLO":
         # 2) ALICE TRANSMITE SUA CHAVE PÙBLICA 
         # -- troque string CHAVE PUBLICA pela chave publica em formato base64 (e remova o encode)
-
-        s.sendto(RSA.converteChavePublica(public_pem="chavepriva.pem"), addr )
+        s.sendto(chavePubPEM, addr )
 
         # 3) ALICE RECEBE A CHAVE SECRETA DE BOB (em formato PEM) criptografada
         chaveCifrada, addr = s.recvfrom(1024)
-        print(f'RECEBI uma chave cifrada de {addr}')
+        print(f'Recebi uma chave cifrada de {addr}')
 
         # 4) ALICE DESCRIPTOGRAFA A chave secreta e converte para binário
         # observe que a chave não é texto, por isso use a opção text=False
         chaveSecreta = None
 
-        # 5) ALICE criptografa uma mensagem para BOM usando essa chave
+        # 5) ALICE criptografa uma mensagem para BOB usando essa chave
         mensagem = 'OLA! Voce criou um canal criptografado com a ALICE.'
+        ciphertext = 'BOB NAO FEZ O EXERCICIO'.encode()
 
         # 6) ALICE envia a mensagem cifrada para BOB
         # -- troque a string MENSAGEM CIFRADA pela variavel com a mensagem criptografada em base64 (e remova o encode)
-        s.sendto('MENSAGEM CIFRADA'.encode(), addr )       
+        #s.sendto('MENSAGEM CIFRADA'.encode(), addr )  
+        s.sendto(ciphertext, addr )   
+        print(f'Envie uma mensagem cifrada para {addr}')   
    
     else: 
         print('descartei uma mensagem de ', addr)
--- criptografia/somativa1/somativa1/Bob.py
+++ criptografia/somativa1/somativa1/Bob.py
@@ -2,6 +2,7 @@ import socket
 import RSALib as RSA
 import AESLib as AES
 from base64 import b64decode
+import time
 
 ipServidor = '127.0.0.1'
 portaServidor = 9999
@@ -12,42 +13,61 @@ print('ESTA TELA PERTENCE A BOB')
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-s.sendto('HELLO'.encode(), destino )
-   
-print('Aguardando chave pública ...')
+# Neste loop BOB tenta contato com Alice
+while True:
+    # Tenta contato com Alice
+    s.sendto('HELLO'.encode(), destino)
+
+    print('Aguardando chave pública ...')
+
+    try:
+        chavePubPEM, addr = s.recvfrom(1024)
+        print('Recebi uma chave pública')
+        print('Chave Publica:', chavePubPEM)
+        break
+    except:
+        print('Alice não responde!')
+        time.sleep(5)
 
 
+try:
+    
 # 1) BOB RECEBE A CHAVE PÚBLICA DE ALICE
 #   -- é preciso converter a chave de base64 para o objeto do Python
-chavePubPEM, addr = s.recvfrom(1024) 
-print('RECEBI uma chave pública')
-chavePubObj2 = AES.decifraMensagem(chavePubPEM)
+    chavePubPEM, addr = s.recvfrom(1024) 
+    print('RECEBI uma chave pública')
+    chavePubObj = RSA.converteChavePublica(chavePubPEM)
 
 # 2) BOB GERA UMA CHAVE SECRETA ALEATÓRIA
-chavesecreta, chavePEM = AES.geraChave(128)
+    chavesecreta, chavePEM = AES.geraChave(128)
 
 # 3) BOB CRIPTOGRAFA A CHAVE SECRETA (EM BYTES) COM A CHAVE PÚBLICA DA ALICE
 #  -- observe que nesse caso, a opção é text=False
-AES.cifraMensagem(chavesecreta, chavePubObj2, text= True)
+    ChaveCifrada = AES.cifraMensagem(chavesecreta, chavePubObj, text= True)
 
 # 4) BOB ENVIA A CHAVE SECRETA CRIPTOGRAFADA PARA ALICE
 #    -- é preciso trocar a string CHAVE SECRETA pela chave secreta criptografada em formato base64 (remova o encode())
 
-s.sendto(RSA.converteChavePublica(chavesecreta), destino )
+    s.sendto(RSA.converteChavePublica(chavesecreta), destino )
+    print(f'Enviei uma chave secreta para {addr}')
+    print('Chave Cifrada:', ChaveCifrada)
 
 # 5) BOB DESCRIPTOGRAFA UMA MENSAGEM CIFRADA DA ALICE
 # -- é preciso descriptografar o ciphertext e salvar em plain text!!!
-ciphertext, addr = s.recvfrom(1024) 
-print('RECEBI uma mensagem cifrada')
+    ciphertext, addr = s.recvfrom(1024) 
+    print(f'RECEBI uma mensagem cifrada de {addr}')
+    print('Ciphertext:', ciphertext)
+    
+    plaintext = AES.decifraMensagem(ciphertext)
 
-AES.decifraMensagem(ciphertext)
-
-plaintext = 'EU NAO FIZ O EXERCICIO'
+    plaintext = 'EU NAO FIZ O EXERCICIO'
 
 #------------------------------------------------
 # SE VOCE FEZ TUDO CERTO A MENSAGEM DA ALICE VAI SER IMPRESSA AQUI
-print(plaintext)
+    print('Plaintext:', plaintext)
 
+except Exception as e:
+    print(e)
 
 
 
